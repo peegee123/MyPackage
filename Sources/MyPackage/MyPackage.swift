07dfd3fe0536148fd770ef8f5bafa0e05abdd925
3firemusic <peter.woods@3firemusic.com>
2025-01-20 16:48:37 +0000

    Initial commit

---
+++ Sources/MyPackage/MyPackage.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct MyPackage {
+    public static func sayHello() -> String {
+        return "Hello from MyPackage 2!"
+    }
+}
